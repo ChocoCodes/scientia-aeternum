c0e29c22ee4a86a1e1894246c89ba200c01a5737
ChocoCodes <chocojhong999@gmail.com>
2025-01-08 22:19:48 +0800

    Chore: main page styling

--- src/components/component/Hero.tsx
+++ src/components/component/Hero.tsx
@@ -2,7 +2,7 @@ export default function Hero() {
     return (
         <section className="hero">
             <h3 id="blog">Blog</h3>
-            <h3 id="title">Webpage of Significant Learning</h3>
+            <p id="title">Webpage of Significant Learning</p>
             <p>A curated list of key takeaways and reflection for the course Science, Technology, and Society `23</p>
         </section>
     );
--- src/utils/contents.ts
+++ src/utils/contents.ts
@@ -1,42 +1,41 @@
 import { ModuleContent } from './utils';
 import textBlocks from './textblocks';
 
-// ./src/ - TODO: prefix 
 const contents: ModuleContent[] = [
     {
         module: 1,
         title: 'A Historical Odyssey — Science and Technology Through the Years',
-        image: 'assets/img/m1.png',
+        image: './src/assets/img/m1.png',
         content: textBlocks[0],
     },
     {
         module: 2,
         title: 'Impacts of Intellectual Revolutions in the Society',
-        image: 'assets/img/m2.png',
+        image: './src/assets/img/m2.png',
         content: textBlocks[1]
     },
     {
         module: 3,
         title: 'Exploring Diversity in the society and the UN Sustainable Goals',
-        image: 'assets/img/m3.png',
+        image: './src/assets/img/m3.png',
         content: textBlocks[2]
     },
     {
         module: 4,
         title: 'Unveiling the Dynamics of Biodiversity and Environmental Issues',
-        image: 'assets/img/m45.png',
+        image: './src/assets/img/m45.png',
         content: textBlocks[3]
     },
     {
         module: 5,
         title: 'Enabling Human Flourishing in the Modern Age of Science and Technology',
-        image: 'assets/img/m6.png',
+        image: './src/assets/img/m6.png',
         content: textBlocks[4]
     },
     {
         module: 6,
         title: 'Student Reflection on the Course',
-        image: 'assets/img/ref.jpg',
+        image: './src/assets/img/ref.jpg',
         content: textBlocks[5]
     },
 ];
